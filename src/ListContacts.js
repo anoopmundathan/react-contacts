e2ac6801d7f9c02974b46277974d46b6da8914fb
anoopmundathan <anoop.mundathan@gmail.com>
2017-07-27 05:09:50 +0100

    Remove contact

---
+++ src/ListContacts.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ListContacts extends Component {
+	
+
+	render() {
+
+		return(
+			<ol className='contact-list'>
+				{this.props.contacts.map(contact => (
+					<li key={contact.id} className='contact-list-item'>
+						
+						<div className='contact-avatar' style={{
+							backgroundImage: `url(${contact.avatarURL})`
+						}}>
+						</div>
+
+						<div className='contact-details'>
+							<p>{contact.name}</p>
+							<p>{contact.email}</p>
+						</div>
+
+						<button 
+							className='contact-remove'
+							onClick={() => this.props.onDeleteContact(contact)}/>
+					</li>
+				))}
+			</ol>
+		);
+	}
+}
+
+export default ListContacts;
